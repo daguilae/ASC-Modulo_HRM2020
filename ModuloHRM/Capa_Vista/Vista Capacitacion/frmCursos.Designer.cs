178fe0d487b14f238bb1d7ebf692ad70d5d63f96
HOXA <rsandovale@miumg.edu.gt>
2020-11-07 03:02:01 -0600

    Capacitacion final 2.0

--- ModuloHRM/Capa_Vista/Vista Capacitacion/frmCursos.Designer.cs
+++ ModuloHRM/Capa_Vista/Vista Capacitacion/frmCursos.Designer.cs
@@ -452,7 +452,7 @@
             // 
             // btnBuscarCap
             // 
-            this.btnBuscarCap.Location = new System.Drawing.Point(373, 13);
+            this.btnBuscarCap.Location = new System.Drawing.Point(373, 10);
             this.btnBuscarCap.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btnBuscarCap.Name = "btnBuscarCap";
             this.btnBuscarCap.Size = new System.Drawing.Size(83, 23);
